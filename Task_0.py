7037f733d29cbc93dee2a85016a236f3b624df04
valik <valikd836@gmail.com>
2021-06-13 19:49:53 +0300

    first commit

---
+++ Task_0.py
@@ -0,0 +1,13 @@
+from random import randint
+
+numbers = []
+for i in range(30):
+    numbers.append(randint(-100, 100))
+maxnumber = max(numbers)
+print(numbers)
+print("Максимальне число =", maxnumber)
+print("Порядковий номер максимального числа =", int(numbers.index(maxnumber))+1)
+print("Пари від’ємних чисел, що стоять поруч:")
+for i in range(29):
+  if numbers[i] < 0 and numbers[i+1] < 0:
+    print(numbers[i], numbers[i+1])
